1255dde3f1b850deded64f18aeed6b4c3d70e6d0
Griffith <davis.griffith@phenom.com>
2024-05-07 17:23:16 +0530

    test cases added

--- test.java
+++ test.java
@@ -2,38 +2,42 @@
 public class test {
 
     public static void main(String[] args) {
-        lowerToUpper("griffith");
-        lowerToUpper("GrEgOrY");
-        lowerToUpper("s");
+        tc1();
     }
-    
-    public static void lowerToUpper(String s){
+
+    public static String lowerToUpper(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-        if(isLetter(s.charAt(i)) && isLower(s.charAt(i)))
-        {
-            char c = (char) (s.charAt(i) -32);
-            sb.append(c);
+        for (int i = 0; i < s.length(); i++) {
+            if (isLetter(s.charAt(i)) && isLower(s.charAt(i))) {
+                char c = (char) (s.charAt(i) - 32);
+                sb.append(c);
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
-        else
-        {
-            sb.append(s.charAt(i));
-        }
-        }
-        System.out.println(sb.toString());
-        }
+        return sb.toString();
+    }
 
-    public static boolean isLetter(char s){
-        if(s >= 97 && s<=122 || s>= 65 && s<=98){
+    public static boolean isLetter(char s) {
+        if (s >= 97 && s <= 122 || s >= 65 && s <= 98) {
             return true;
         }
         return false;
     }
 
-    public static boolean isLower(char s){
-        if(s >= 97 && s<=122){
+    public static boolean isLower(char s) {
+        if (s >= 97 && s <= 122) {
             return true;
         }
         return false;
+    }
+
+
+    public static boolean tc1(){
+        String expected = "GRIFFITH";
+        String input1 = "griffith";
+        String result = lowerToUpper(input1);
+        System.out.printf(expected, result);
+        return true;
     }
 }
