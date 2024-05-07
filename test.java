12ef434b90c4edbd9b1687d33bb8f4066d82bf2b
Griffith <davis.griffith@phenom.com>
2024-05-07 18:05:25 +0530

    4 test cases added

--- test.java
+++ test.java
@@ -1,8 +1,10 @@
-
 public class test {
 
     public static void main(String[] args) {
-        tc1();
+        System.out.println(tc1());
+        System.out.println(tc2());
+        System.out.println(tc3());
+        System.out.println(tc4());
     }
 
     public static String lowerToUpper(String s) {
@@ -33,11 +35,56 @@ public class test {
     }
 
 
+    /*Test case to check if the sting is being converted to Upper Case */
     public static boolean tc1(){
         String expected = "GRIFFITH";
         String input1 = "griffith";
         String result = lowerToUpper(input1);
-        System.out.printf(expected, result);
-        return true;
+        if(expected.equals(result))
+        {
+            return true;
+        }
+        else
+        return false;
+    }
+
+
+    /*Test case to check for an empty string*/
+    public static boolean tc2()
+    {
+        String expected = "";
+        String input2 = "";
+        String res = lowerToUpper(input2);
+        if(expected.equals(res))
+        {
+            return true;
+        }
+        else
+        return false;
+    }
+
+    /*Test case to check conversion of string including special characters and numbers */
+    public static boolean tc3()
+    {
+        String expected = "GRIFFITH@123";
+        String input3="griffith@123";
+        String result = lowerToUpper(input3);
+        if (expected.equals(result)) {
+            return true;
+        }
+        else
+        return false;
+    }
+
+    /*Test case to check conversion of a string that also has numbers */
+    public static boolean tc4()
+    {
+        String expected = "HELLO123";
+        String input = "Hello123 ";
+        String result = lowerToUpper(input);
+        if (expected.equals(result)) {
+            return true;
+        }
+        return false;
     }
 }
